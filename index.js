be6eb232823f6fe631d7a272a0e1ee49735bb621
ThiagoMejias <mejiasthiago1@gmail.com>
2022-10-09 17:22:08 -0300

    header resposive

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
+// const carruzel = document.querySelector("#carruzel");
 
+// let imagenes = document.querySelectorAll(".conteiner-img");
+// let ultimaImg =  imagenes[imagenes.length - 1];
 
+// const btnIzquierda = document.getElementById("boton-izquierda");
+// const btnDerecha = document.getElementById("boton-derecha");
 
-const carruzel = document.querySelector("#carruzel");
+// carruzel.insertAdjacentElement('afterbegin',ultimaImg);
 
-let imagenes = document.querySelectorAll(".conteiner-img");
-let ultimaImg =  imagenes[imagenes.length - 1];
+// const btnMenu = document.querySelector(".container-burguer");
+// console.log(btnMenu);
+// const nav = document.querySelector(".nav-celular");
+// const lista = document.querySelector(".lista_enlaces_celular");
 
-const btnIzquierda = document.getElementById("boton-izquierda");
-const btnDerecha = document.getElementById("boton-derecha");
+// btnMenu.addEventListener("click", () => {
+//   console.log(nav);
+//   if (nav.style.display != "flex") {
+//     nav.style.display = "flex";
+//     lista.style.display = "flex";
+//     lista.style.flexDirection = "column";
+//     nav.style.flexDirection = "column";
+//   } else nav.style.display = "none";
+// });
 
-carruzel.insertAdjacentElement('afterbegin',ultimaImg);
+const line1__bars = document.querySelector(".line1__barras-menu");
+const line2__bars = document.querySelector(".line2__barras-menu");
+const line3__bars = document.querySelector(".line3__barras-menu");
+const nav = document.querySelector(".nav-celular");
+nav.style.display = "none";
+
+document
+  .querySelector(".barras_menu")
+  .addEventListener("click", menuInteractivo);
+
+document.querySelector(".barras_menu").addEventListener("click", animateBars);
+
+function animateBars() {
+  line1__bars.classList.toggle("activeline1__barras-menu");
+  line2__bars.classList.toggle("activeline2__barras-menu");
+  line3__bars.classList.toggle("activeline3__barras-menu");
+}
+function menuInteractivo() {
+  if (nav.style.display == "none") {
+    nav.style.display = "block";
+  } else nav.style.display = "none";
+}
