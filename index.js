8b7fa7d6bd28b3b7d403d38008e6510027af4d97
ThiagoMejias <mejiasthiago1@gmail.com>
2022-10-11 14:24:04 -0300

    Responsive de la galeria

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-// const carruzel = document.querySelector("#carruzel");
-
-// let imagenes = document.querySelectorAll(".conteiner-img");
-// let ultimaImg =  imagenes[imagenes.length - 1];
-
-// const btnIzquierda = document.getElementById("boton-izquierda");
-// const btnDerecha = document.getElementById("boton-derecha");
-
-// carruzel.insertAdjacentElement('afterbegin',ultimaImg);
-
-// const btnMenu = document.querySelector(".container-burguer");
-// console.log(btnMenu);
-// const nav = document.querySelector(".nav-celular");
-// const lista = document.querySelector(".lista_enlaces_celular");
-
-// btnMenu.addEventListener("click", () => {
-//   console.log(nav);
-//   if (nav.style.display != "flex") {
-//     nav.style.display = "flex";
-//     lista.style.display = "flex";
-//     lista.style.flexDirection = "column";
-//     nav.style.flexDirection = "column";
-//   } else nav.style.display = "none";
-// });
-
-// header//
 const line1__bars = document.querySelector(".line1__barras-menu");
 const line2__bars = document.querySelector(".line2__barras-menu");
 const line3__bars = document.querySelector(".line3__barras-menu");
@@ -39,9 +13,6 @@ function cerrarAbrirMenu() {
   line2__bars.classList.toggle("activeline2__barras-menu");
   line3__bars.classList.toggle("activeline3__barras-menu");
   nav.classList.toggle("active-header");
-  // nav.style.visibility == "hidden"
-  //   ? (nav.style.visibility = "visible")
-  //   : (nav.style.visibility = "hidden");
 }
 // header
 
@@ -51,16 +22,20 @@ const btnDerecha = document.getElementById("btnDerecha");
 const btnIzquierda = document.getElementById("btnIzquierda");
 
 let imagenActual = contenedor.childNodes[1];
-const btnAdelante = document.getElementById;
 imagenActual.style.display = "block";
-console.log();
+const btnAdelante = document.getElementById;
+//imagenActual.classList.toggle("active-img");
+
 btnDerecha.addEventListener("click", adelante);
 btnIzquierda.addEventListener("click", atras);
+
 function adelante() {
   imagenActual.style.display = "none";
+
   imagenActual.nextElementSibling != null
     ? (imagenActual = imagenActual.nextElementSibling)
     : (imagenActual = contenedor.firstElementChild);
+
   imagenActual.style.display = "block";
 }
 
@@ -69,7 +44,6 @@ function atras() {
   imagenActual.previousElementSibling != null
     ? (imagenActual = imagenActual.previousElementSibling)
     : (imagenActual = contenedor.lastElementChild);
-
   imagenActual.style.display = "block";
 }
 
