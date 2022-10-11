95ccbb1fb3cea29fdca5c80e4a79a04314005309
ThiagoMejias <mejiasthiago1@gmail.com>
2022-10-11 13:08:20 -0300

    Agregado de galeria y solucion del bug header

--- index.js
+++ index.js
@@ -23,14 +23,13 @@
 //   } else nav.style.display = "none";
 // });
 
+// header//
 const line1__bars = document.querySelector(".line1__barras-menu");
 const line2__bars = document.querySelector(".line2__barras-menu");
 const line3__bars = document.querySelector(".line3__barras-menu");
 const nav = document.querySelector(".nav-celular");
-// nav.style.display = "none";
 
 nav.addEventListener("click", cerrarAbrirMenu);
-
 document
   .querySelector(".barras_menu")
   .addEventListener("click", cerrarAbrirMenu);
@@ -40,9 +39,38 @@ function cerrarAbrirMenu() {
   line2__bars.classList.toggle("activeline2__barras-menu");
   line3__bars.classList.toggle("activeline3__barras-menu");
   nav.classList.toggle("active-header");
-  // if (nav.style.display == "none") {
-  //   nav.style.display = "block";
-  // } else {
-  //   nav.style.display = "none";
-  // }
+  // nav.style.visibility == "hidden"
+  //   ? (nav.style.visibility = "visible")
+  //   : (nav.style.visibility = "hidden");
 }
+// header
+
+// galeria
+const contenedor = document.querySelector(".container-imagenes");
+const btnDerecha = document.getElementById("btnDerecha");
+const btnIzquierda = document.getElementById("btnIzquierda");
+
+let imagenActual = contenedor.childNodes[1];
+const btnAdelante = document.getElementById;
+imagenActual.style.display = "block";
+console.log();
+btnDerecha.addEventListener("click", adelante);
+btnIzquierda.addEventListener("click", atras);
+function adelante() {
+  imagenActual.style.display = "none";
+  imagenActual.nextElementSibling != null
+    ? (imagenActual = imagenActual.nextElementSibling)
+    : (imagenActual = contenedor.firstElementChild);
+  imagenActual.style.display = "block";
+}
+
+function atras() {
+  imagenActual.style.display = "none";
+  imagenActual.previousElementSibling != null
+    ? (imagenActual = imagenActual.previousElementSibling)
+    : (imagenActual = contenedor.lastElementChild);
+
+  imagenActual.style.display = "block";
+}
+
+// galeria
